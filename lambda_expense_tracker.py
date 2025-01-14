88c76ac39adf91dbe42a563b0d09351b99fc4cc3
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-15 01:52:08 +0200

    added a new file with sample codes on lists

---
+++ lambda_expense_tracker.py
@@ -0,0 +1,18 @@
+"""This project using lambda functions in python to build an expenses tracker"""
+
+# first, we will ook at how lists work, for revision 
+my_list = [1, 2]
+
+my_list.append(3)
+print(my_list)
+
+print(my_list[0])
+
+my_list[0] = 0
+print(my_list)
+
+my_list.insert(1, 1)
+print(my_list)
+
+my_list.pop()
+print(my_list)
