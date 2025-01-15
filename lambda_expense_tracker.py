a2d832d40e891bc136d71d57f29568b7b7639c4b
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-15 02:46:09 +0200

    updates

--- lambda_expense_tracker.py
+++ lambda_expense_tracker.py
@@ -30,8 +30,18 @@ def total_expenses(expenses):
 
 
 def filter_expenses_by_category(expenses, category):
-    pass
+    return filter(lambda expense: expense['category'] == category, expenses)
 
+def main():
+    expenses = []
+
+    while True:
+        print('\nExpense Tracker')
+        print('1. Add an expense')
+        print('2. List all expenses')
+        print('3. Show total expenses')
+        print('4. Filter expenses by category')
+        print('5. Exit')
 # lambda are simple unknownimous functions that are idea for one time jobs .. here is an example 
 
 test = lambda x: x *2 
