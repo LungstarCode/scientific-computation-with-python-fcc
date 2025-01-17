37a2b98a7004afa43b0a750b4ec60e6b9367a847
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-17 03:58:32 +0200

    added the square root bisection function implementation

--- square_root_bisection.py
+++ square_root_bisection.py
@@ -1,4 +1,11 @@
 
 
 def square_root_bisection(square_target, tolerance=1e-7, max_iterations= 100):
-    pass
+    if square_target < 0:
+        raise ValueError('square root of a negative number is not defined in real numbers!')
+    
+    if square_target == 1:
+        print(f'The square root of {square_target} is 1')
+
+    elif square_target == 0:
+        print(f'The square root of {square_target} is 0')
