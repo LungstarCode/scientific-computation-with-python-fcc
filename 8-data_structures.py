2c869a09f5c565fe6145922be08d0aad16f49bf6
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-19 22:27:26 +0200

    competed the sorting algorithm

--- 8-data_structures.py
+++ 8-data_structures.py
@@ -1,4 +1,7 @@
 def merge_sort(array):
+
+    if len(array) <= 1:
+        return
     
     middle_point = len(array) // 2
     left_part = array[:middle_point]
@@ -22,7 +25,13 @@ def merge_sort(array):
         
     while left_array_index < len(left_part):
         array[sorted_index] = left_part[left_array_index]
-
+        left_array_index += 1
+        sorted_index += 1
+    
+    while right_array_index < len(right_part):
+        array[sorted_index] = right_part[right_array_index]
+        right_array_index += 1
+        sorted_index += 1
 
 
 if __name__ == '__main__':
