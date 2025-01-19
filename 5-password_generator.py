a23cc451c892c4152b89e7df790416bc82cff72a
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-19 02:36:08 +0200

    finalized the shortest distance algorithm

--- 5-password_generator.py
+++ 5-password_generator.py
@@ -29,3 +29,4 @@ def generate_password(length=16, nums=1, special_chars=1 , uppercase=1, lowercas
 if __name__ == "__main__":
     new_password = generate_password()
     print("Password Generated:", new_password)
+    
