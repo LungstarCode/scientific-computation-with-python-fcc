fbae92675fe169e47ae598dcee9abc686e091eb9
Lungile Mawila <lungilemawila5@gmail.com>
2025-01-24 00:17:39 +0200

    updated

--- concepts.py
+++ concepts.py
@@ -1,7 +1,39 @@
+import string 
+import secrets
+import re
 
 
-sum = lambda n, e : n + e
+def generate_password(length, nums, lowercase, uppercase, chars):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
 
-print(sum(2, 3))
+    pattern = letters + digits + special
+
+    password = ''
+    
+
+    while True:   
+        constraints = {
+        (nums, r'\d'),
+        (lowercase, r'[a-z]'),
+        (chars, rf'[{special}]'),
+        (uppercase, r'[A-Z]'),
+    }
+
+        for _ in range(length):
+            password += secrets.choice(pattern)
+
+        if all(
+            contraint <= all(re.findall(pattern , password))
+
+            for contraint , pattern in constraints):
+                break
+    return password
+
+
+print(generate_password(16, 1,1 ,1,1))
+
+
 
 
